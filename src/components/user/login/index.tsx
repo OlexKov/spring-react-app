431e6d940563308db116ef3d1157549c5d9e99e2
OlexKov <Kovalchuk.olex@gmail.com>
2024-09-30 14:25:26 +0300

    Add recaptcha verify to login

--- src/components/user/login/index.tsx
+++ src/components/user/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 import { accountService } from '../../../services/accountService';
 import { Button, Checkbox, Divider, Form, Input, message } from 'antd';
@@ -12,11 +12,28 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 import { APP_ENV } from '../../../env';
 import GoogleLoginButton from '../../buttons/google-login-button';
 import { GoogleOutlined } from '@ant-design/icons';
+import { GoogleReCaptcha, GoogleReCaptchaProvider} from 'react-google-recaptcha-v3';
 
 export const Login: React.FC = () => {
     const [remember, setRemember] = useState<boolean>(false);
     const navigate = useNavigate();
     const dispatcher = useDispatch();
+    const [loginModel, setLoginModel] = useState<LoginModel>({username:"",password:"",recaptchaToken:""});
+
+    const onVerify = useCallback(async (token: string) => {
+        if (token != "") {
+            loginModel.recaptchaToken = token;
+            const responce = await accountService.login(loginModel);
+            if (responce.status === 200) {
+                await login(responce.data.token)
+            }
+        }
+        else {
+            message.success('reCaptcha перевірка не пройдена')
+        }
+    }, [loginModel]);
+
+
 
     const login = async (token: string) => {
         await storageService.saveToken(token, !remember);
@@ -36,79 +53,79 @@ export const Login: React.FC = () => {
             navigate('/')
             message.success('Ви успішно увійшли в свій акаунт')
         }
-        else{
+        else {
             message.success('Помилка авторизації')
         }
     }
-
-    const onFinish = async (loginModel: LoginModel) => {
-        const responce = await accountService.login(loginModel);
-        if (responce.status === 200) {
-            await login(responce.data.token)
-        }
-    }
+   
     return (
+
         <GoogleOAuthProvider clientId={APP_ENV.CLIENT_ID}>
-            <div className=' w-70 mx-auto my-4'>
-            </div>
-            <div className='w-50 mx-auto'>
-                <Divider className='fs-3 border-dark-subtle mb-5' orientation="left">Логін</Divider>
-                <Form
-                    layout='vertical'
-                    style={{
-                        maxWidth: 600,
-                    }}
-                    initialValues={{
-                        remember: true,
-                    }}
-                    onFinish={onFinish}
-                    className='mx-auto'
-                >
-                    <Form.Item
-                        label="Логін"
-                        name="username"
-                        rules={[
-                            {
-                                required: true,
-                                message: "Будьласка введіть логін!",
-                            }
-                        ]}
-                    >
-                        <Input type='large' />
-                    </Form.Item>
+            <GoogleReCaptchaProvider reCaptchaKey={APP_ENV.RECAPTCHA_SITE_KEY}>
+                <div className=' w-70 mx-auto my-4'>
+                    <div className='w-50 mx-auto'>
+                        <Divider className='fs-3 border-dark-subtle mb-5' orientation="left">Логін</Divider>
 
-                    <Form.Item
-                        label="Пароль"
-                        name="password"
-                        rules={[
-                            {
-                                required: true,
-                                message: 'Будьласка введіть пароль!',
-                            },
-                        ]}
-                    >
-                        <Input.Password type='large' />
-                    </Form.Item>
+                        <Form
+                            layout='vertical'
+                            style={{
+                                maxWidth: 600,
+                            }}
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={setLoginModel}
+                            className='mx-auto'
+                        >
+                            <Form.Item
+                                label="Логін"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: "Будьласка введіть логін!",
+                                    }
+                                ]}
+                            >
+                                <Input type='large' />
+                            </Form.Item>
 
-                    <Form.Item
-                        // valuePropName='checked'
-                        name='remember'>
-                        <Checkbox checked={remember} onChange={(e: CheckboxChangeEvent) => setRemember(e.target.checked)}>Запам'ятати мене</Checkbox>
-                    </Form.Item>
-                    <div className='buttons-block'>
-                        <Button type="primary" htmlType="submit">
-                            Увійти
-                        </Button>
-                        <Link to="/registration">
-                            <Button >Реєстрація</Button>
-                        </Link>
-                        <Link to='/fogotpassword'>Забули раполь?</Link>
-                        <GoogleLoginButton icon={<GoogleOutlined />} title='Увійти з Google' onLogin={login} />
+                            <Form.Item
+                                label="Пароль"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Будьласка введіть пароль!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password type='large' />
+                            </Form.Item>
+
+                            <Form.Item
+                                // valuePropName='checked'
+                                name='remember'>
+                                <Checkbox checked={remember} onChange={(e: CheckboxChangeEvent) => setRemember(e.target.checked)}>Запам'ятати мене</Checkbox>
+                            </Form.Item>
+                            <div className='buttons-block'>
+                                <Button type="primary" htmlType="submit">
+                                    Увійти
+                                </Button>
+                                <Link to="/registration">
+                                    <Button >Реєстрація</Button>
+                                </Link>
+                                <Link to='/fogotpassword'>Забули раполь?</Link>
+                                <GoogleLoginButton icon={<GoogleOutlined />} title='Увійти з Google' onLogin={login} />
+                            </div>
+                            <GoogleReCaptcha onVerify={onVerify}/>
+                        </Form>
                     </div>
-                </Form>
-            </div>
+                </div>
+            </GoogleReCaptchaProvider>
 
-        </GoogleOAuthProvider>
+        </GoogleOAuthProvider >
+
     )
 }
 
